3ebd382ad996960f36b782f7046bd301e15212f8
Paul Grey <greyp9@yahoo.com>
2021-04-27 14:09:25 -0400

    NIFI-8478; expose surefire output; trace locale

--- nifi-commons/nifi-utils/src/test/java/org/apache/nifi/util/locale/TestLocaleOfTestSuite.java
+++ nifi-commons/nifi-utils/src/test/java/org/apache/nifi/util/locale/TestLocaleOfTestSuite.java
@@ -1,9 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nifi.util.locale;
 
 import org.junit.Assume;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 /**
@@ -20,9 +37,12 @@ public class TestLocaleOfTestSuite {
         final String userLanguage = System.getProperty("user.language");
         final String userCountry = System.getProperty("user.country");
         final String userRegion = System.getProperty("user.region");
-        logger.warning(String.format("Test environment: user.language=[%s], user.country=[%s], user.region=[%s]",
-                userLanguage, userCountry, userRegion));
+        String languageTag = Locale.getDefault().toLanguageTag();
+        logger.warning(String.format(
+                "Test environment: locale=[%s] user.language=[%s], user.country=[%s], user.region=[%s]",
+                languageTag, userLanguage, userCountry, userRegion));
         Assume.assumeTrue(Arrays.asList("en", "fr", "ja").contains(userLanguage));
-        Assume.assumeTrue(Arrays.asList("AU", "FR", "JP").contains(userCountry));
+        Assume.assumeTrue(Arrays.asList("US", "AU", "FR", "JP").contains(userCountry));
+        Assume.assumeTrue(Arrays.asList("en-US", "fr-FR").contains(languageTag));
     }
 }
